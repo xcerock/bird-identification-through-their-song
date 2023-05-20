65b46e9552c8d799de69b59500578c4137a8f21b
unknown <s.ramirez10@utp.edu.co>
2023-05-20 15:15:29 -0500

    the menssages

--- preprocesamiento_de_datos_prueba.py
+++ preprocesamiento_de_datos_prueba.py
@@ -7,16 +7,10 @@ carpeta_audio = 'C:\\Users\\lenovo\\OneDrive\\Escritorio\\proyecto\\datos_de_pru
 archivos_audio = os.listdir(carpeta_audio)
 n_fft = 1024
 num_caracteristicas_seleccionadas = 3
-etiquetas = ['Bulbul  naranjero', 'Candelita plomiza', 'Carbonero común', 'Cotorrita aliazul',
-             'cucarachero pechihabano', 'cuclillo piquinegro', 'Fiofío gris del Atlántico',
-             'mielero escamoso', 'Milano muslirrufo', 'Minero rojizo', 'Mirlo común', 'Pava amazónica',
-             'Pijuí frentigrís', 'Tirahojas ogarití', 'Trompetero aliverde occidental', 'Tucán pechiblanco',
-             'Urraca negra malaya', 'Zarcero páñido']
 
-caracteristicas_seleccionadas = []
-correlaciones = {}
+correlaciones = []
 
-for archivo, etiqueta in zip(archivos_audio, etiquetas):
+for archivo in archivos_audio:
     ruta_audio = os.path.join(carpeta_audio, archivo)
 
     try:
@@ -29,22 +23,11 @@ for archivo, etiqueta in zip(archivos_audio, etiquetas):
         std_media_frecuencias = np.std(media_frecuencias)
 
         if std_media_frecuencias != 0:
-            if etiqueta not in correlaciones:
-                correlaciones[etiqueta.strip()] = []
-
-            correlaciones[etiqueta.strip()].extend(np.sort(media_frecuencias)[-num_caracteristicas_seleccionadas:])
+            correlaciones.append(np.sort(media_frecuencias)[-num_caracteristicas_seleccionadas:])
 
     except Exception as e:
         print(f"Error al procesar el archivo {archivo}: {str(e)}")
 
-etiquetas_numericas = {etiqueta: indice for indice, etiqueta in enumerate(set(etiquetas))}
-etiquetas_convertidas = [etiquetas_numericas[etiqueta] for etiqueta in etiquetas]
-
-etiquetas_convertidas = np.array(etiquetas_convertidas)
-
-# Convertir a tensores de PyTorch
-input_data = torch.tensor(list(correlaciones.values()), dtype=torch.float32)
-targets = torch.tensor(etiquetas_convertidas, dtype=torch.long)
+input_data = torch.tensor(np.array(correlaciones), dtype=torch.float32)
 
 print("Dimensiones de input_data:", input_data.size())
-print("Dimensiones de targets:", targets.size())
