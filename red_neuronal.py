6e08e1540da9701dc1839a1ff73e3491cfabcb8f
unknown <s.ramirez10@utp.edu.co>
2023-05-21 00:39:24 -0500

    the menssages

--- red_neuronal.py
+++ red_neuronal.py
@@ -56,12 +56,15 @@ class BirdClassificationNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(BirdClassificationNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
+        self.dropout = nn.Dropout(0.5)  # Agregar una capa de dropout con una probabilidad de 0.5
         self.fc2 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
+        x = self.dropout(x)  # Aplicar dropout después de la primera capa oculta
         x = self.fc2(x)
         return x
+
 
 input_size = input_data.size(1)  # Size of the input layer
 hidden_size = 64
