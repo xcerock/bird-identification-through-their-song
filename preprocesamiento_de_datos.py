7cfd67a7e045b2d69f2035918279dfba6b8ca2bf
unknown <s.ramirez10@utp.edu.co>
2023-05-20 23:41:11 -0500

    the menssages

--- preprocesamiento_de_datos.py
+++ preprocesamiento_de_datos.py
@@ -3,7 +3,6 @@ import librosa
 import numpy as np
 import torch
 
-
 carpeta_audio = 'C:\\Users\\lenovo\\OneDrive\\Escritorio\\proyecto\\datos_de_entrenamiento'
 archivos_audio = os.listdir(carpeta_audio)
 n_fft = 1024
@@ -22,29 +21,25 @@ for archivo, etiqueta in zip(archivos_audio, etiquetas):
 
     try:
         audio, sr = librosa.load(ruta_audio)
-        audio = librosa.util.normalize(audio)
         stft = librosa.stft(audio, n_fft=n_fft)
         magnitud = np.abs(stft)
+        mean = np.mean(magnitud)  # Calcula la media de las magnitudes
+        std = np.std(magnitud)  # Calcula la desviación estándar de las magnitudes
+        magnitud = (magnitud - mean) / std  # Normalización Z-score
         media_frecuencias = np.mean(magnitud, axis=1)
-
-        std_media_frecuencias = np.std(media_frecuencias)
-
-        if std_media_frecuencias != 0:
-            if etiqueta not in correlaciones:
-                correlaciones[etiqueta.strip()] = []
-
-            correlaciones[etiqueta.strip()].extend(np.sort(media_frecuencias)[-num_caracteristicas_seleccionadas:])
+        correlaciones[etiqueta] = np.sort(media_frecuencias)[-num_caracteristicas_seleccionadas:]
 
     except Exception as e:
         print(f"Error al procesar el archivo {archivo}: {str(e)}")
 
+# Convertir etiquetas de texto a valores numéricos
 etiquetas_numericas = {etiqueta: indice for indice, etiqueta in enumerate(set(etiquetas))}
 etiquetas_convertidas = [etiquetas_numericas[etiqueta] for etiqueta in etiquetas]
 
 etiquetas_convertidas = np.array(etiquetas_convertidas)
 
 # Convertir a tensores de PyTorch
-input_data = torch.tensor(list(correlaciones.values()), dtype=torch.float32)
+input_data = torch.tensor(np.array(list(correlaciones.values())), dtype=torch.float32)
 targets = torch.tensor(etiquetas_convertidas, dtype=torch.long)
 
 print("Dimensiones de input_data:", input_data.size())
--- preprocesamiento_de_datos_prueba.py
+++ preprocesamiento_de_datos_prueba.py
@@ -15,19 +15,25 @@ for archivo in archivos_audio:
 
     try:
         audio, sr = librosa.load(ruta_audio)
-        audio = librosa.util.normalize(audio)
         stft = librosa.stft(audio, n_fft=n_fft)
         magnitud = np.abs(stft)
         media_frecuencias = np.mean(magnitud, axis=1)
+        
+        # Calcular la media y la desviación estándar de los datos de prueba
+        mean_pruebas = np.mean(media_frecuencias)
+        std_pruebas = np.std(media_frecuencias)
+        
+        # Aplicar la normalización Z-score a los datos de prueba
+        media_frecuencias_prueba = (media_frecuencias - mean_pruebas) / std_pruebas
 
-        std_media_frecuencias = np.std(media_frecuencias)
+        std_media_frecuencias = np.std(media_frecuencias_prueba)
 
         if std_media_frecuencias != 0:
-            correlaciones_prueba.append(np.sort(media_frecuencias)[-num_caracteristicas_seleccionadas:])
+            correlaciones_prueba.append(np.sort(media_frecuencias_prueba)[-num_caracteristicas_seleccionadas:])
 
     except Exception as e:
         print(f"Error al procesar el archivo {archivo}: {str(e)}")
 
 input_data_pruebas = torch.tensor(np.array(correlaciones_prueba), dtype=torch.float32)
 
-print("Dimensiones de input_data:", input_data_pruebas.size())
+print("Dimensiones de input_data_pruebas:", input_data_pruebas.size())
